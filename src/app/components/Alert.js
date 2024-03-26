551a4dcae6c913c013f56b1838123803683836e2
jcsarmiento <jacinto@011200>
2024-03-26 10:05:09 +0800

    added crud

---
+++ src/app/components/Alert.js
@@ -0,0 +1,14 @@
+function Alert() {
+    return (
+        <>
+        <div class="alert w-50 align-self-center alert-success alert-dismissible fade show" role="alert">
+			Data Added successfully
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+			<span aria-hidden="true">&times;</span>
+		    </button>
+		</div>
+        </>
+    )
+}
+
+export default Alert;
